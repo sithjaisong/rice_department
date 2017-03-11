013d2f6e4fb6e3609ad135c3c4eb1bba115c8217
Sith Jaisong <sith.jaisong@gmail.com>
2017-03-11 13:31:37 +0700

    load fig

--- aom.R
+++ aom.R
@@ -1,18 +1,20 @@
 library(ggplot2)
 library(dplyr)
+library(scales)
 library(multcompView)
 
 data <- read.csv("Antibac+blast.csv")
+data <- read.csv("Antibac+brownspot.csv")
 
 str(data)
 data$X <- NULL
 data$Rep <- as.factor(data$Rep)
-mod1 <- aov(X.inhibition ~ Bac, data)
+mod1 <- aov(inhibition ~ Bac, data)
 Tukey <- TukeyHSD(mod1)
 
-multcompLetters2(X.inhibition ~ Bac, Tukey$Bac[, 4], data)
+multcompLetters2(inhibition ~ Bac, Tukey$Bac[, 4], data)
 
-multcompBoxplot(X.inhibition ~ Bac, data)
+ multcompBoxplot(inhibition ~ Bac, data)
 
 multcompLetters2(y ~ treatments, exp_tukey$treatments[,"p adj"], experiment)
 
@@ -22,10 +24,17 @@ plot(TukeyHSD(fm1, "tension"))
 
 data %>% group_by(Bac) %>% summarise(xbar = mean(X.inhibition)) %>% arrange(desc())
 
-data %>% ggplot() + geom_boxplot(aes(x = reorder(Bac, X.inhibition, mean) , y = X.inhibition)) + 
+data %>% ggplot() + geom_boxplot(aes(x = reorder(Bac, inhibition, mean) , y = inhibition)) + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-data %>% ggplot() + geom_boxplot(aes(x = reorder(Bac, X.inhibition, mean) , y = X.inhibition)) +  coord_flip() + 
-  ylab("Inhibition(%)") + xlab("Isolate")
+number_ticks <- function(n) {function(limits) pretty(limits, n)}
 
-ggsave("ver_anibac.pdf", width = 5, height = 10)multcompLetters(extract_p(TukeyHSD(mod1)))
+data %>% ggplot() + geom_boxplot(aes(x = reorder(Bac, inhibition, mean) , y = inhibition)) +  coord_flip() + 
+  ylab("Inhibition(%)") + xlab("Isolate") + scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +  theme(text = element_text(size=20))
+
+
+
+
+ggsave("fung_vs_brown.pdf", width = 5, height = 10)
+
+multcompLetters(extract_p(TukeyHSD(mod1)))
